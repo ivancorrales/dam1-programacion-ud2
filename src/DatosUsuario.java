583f22f6ad13499acccf9af1d7f6d161991512a9
GarciaMunozCarmen <garciamunozcarmen18@gmail.com>
2025-10-01 14:11:52 +0200

    Ampliación DatosUsuario.java

--- src/DatosUsuario.java
+++ src/DatosUsuario.java
@@ -1,5 +1,9 @@
 /*
- * Este programa recoge los datos del usuario y si el email es válido los muestra
+* Este programa recoge los datos del usuario y si el email es válido los muestra
+*
+* - Si edad es menor que 0 o mayor que 120 mostrar un mensaje de error.
+* - Si el nombre tiene menos de 2 caracteres o mas de 20 mostrar un error
+* - Normalizar el nombre: Primera letra mayuscula, resto en minúscula, eliminar espacios en blanco del nombre
  */
 import java.util.Scanner;
 
@@ -11,17 +15,29 @@ public class DatosUsuario {
         Scanner sc = new Scanner(System.in);
         System.out.print("Introduce tu nombre: ");
         String nombre = sc.nextLine();
+        nombre = nombre.toUpperCase();
+        nombre =(nombre.charAt(0) + nombre.substring(1).toLowerCase()).replace(" ", "");
         System.out.print("Introduce tu email: ");
         String email = sc.nextLine();
         System.out.print("Introduce tu edad: ");
         int edad = sc.nextInt();
-        //Después de pedir los datos comprobamos si el email es válido
+        //Después de pedir los datos comprobamos si los datos son válidos
+        boolean edadValida = edad>0 && edad<120;
+        boolean nombreValido = nombre.length()>2 && nombre.length()<120;
         boolean emailValido = ProcesadorTexto.esEmailValido(email); //utiliza un metodo estático de la clase ProcesadorTexto
-        if(emailValido){
+        if(emailValido && edadValida && nombreValido){
             Usuario usuario = new Usuario(nombre, edad, email); //crea un objeto (variable) nuevo de la clase Usuario
             usuario.mostrarInformacion();
         }else{
-            System.out.println("El usuario no puede ser creado porque el email introducido no es válido");
+            if(edadValida==false){
+                System.out.println("Error: Edad no válida");
+            }
+            if (emailValido==false) {
+                System.out.println("El usuario no puede ser creado porque el email introducido no es válido");
+            }
+            if (nombreValido==false) {
+                System.out.println("Error: Nombre no válido");
+            }
         }
         sc.close();
     }
